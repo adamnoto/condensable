fc6e0689c00a770ec6087d8fd97a4de2fc704427
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-10-24 07:19:33 +0700

    updated spec to test with default value

--- spec/blackhole_spec.rb
+++ spec/blackhole_spec.rb
@@ -35,4 +35,68 @@ describe Blackhole do
       expect(order_params.order_id).to eq("BC23-19238-29910299")
     end
   end
+
+  context "with default behaviour" do
+    context "if unspecified" do
+      it "returns nil" do
+        expect(order_params.adam).to eq(nil)
+      end
+    end
+
+    context "if set to returns a astring" do
+      it "returns string" do
+        class OrderParamsReturnStringC
+          include Blackhole
+          blackhole default: "adam"
+        end
+
+        op = OrderParamsReturnStringC.new
+        expect(op.name).to eq("adam")
+      end
+    end
+
+    context "if set to execute method" do
+      it "executes a method" do
+        class OrderParamsExecMethod 
+          include Blackhole
+          blackhole default: :execute_me
+
+          def execute_me
+            "adam pahlevi"
+          end
+        end
+
+        opem = OrderParamsExecMethod.new
+        expect(opem.name).to eq("adam pahlevi")
+      end
+
+      it "executes a method with params" do
+        class OrderParamsExecMethodParams
+          include Blackhole
+          blackhole default: :execute_me
+
+          def execute_me(name, *args)
+            [name] + args
+          end
+        end
+
+        opemp = OrderParamsExecMethodParams.new
+        expect(opemp.name("adam")).to eq([:name, "adam"])
+        expect(opemp.age).to eq([:age])
+        expect(opemp.whatever("adam", "pahlevi")).to eq([:whatever, "adam", "pahlevi"])
+      end
+    end
+
+    context "if set to raise an error" do
+      it "raises an error" do
+        class OrderParamsRaiseError
+          include Blackhole
+          blackhole default: :raise_error
+        end
+
+        opre = OrderParamsRaiseError.new
+        expect { opre.name }.to raise_error(NoMethodError)
+      end
+    end
+  end
 end
