2d225b1df213e67fc3165f45c831fbf8c8ead818
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-10-28 22:24:38 +0700

    ability to create on-the-fly condensable class

--- lib/condensable.rb
+++ lib/condensable.rb
@@ -10,4 +10,30 @@ module Condensable
     base.include(Condensable::GravityAbsorber)
     base.extend(Condensable::GravityAbsorber::ClassLevelMethods)
   end
+
+  def self.new(*args, &block)
+    # make class on the fly, on the fly
+    klass = Class.new do
+      include Condensable
+    end
+
+    # initialize if args are specified
+    args.each do |arg|
+      if arg.is_a?(Hash)
+        default_value = arg[:default] || arg['default']
+        if default_value
+          klass.class_eval do 
+            condensable default: default_value
+          end
+        end
+      end
+    end
+
+    # if block is given, execute the block
+    if block_given?
+      klass.class_eval(&block)
+    end
+
+    klass
+  end
 end
